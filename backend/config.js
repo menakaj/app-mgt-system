aca5ff3f7b8e047c2f9086bd9ab116885c27f62f
Menaka Jayawardena <menaka12350@gmail.com>
2025-01-07 11:27:46 +0530

    configure connections

--- backend/config.js
+++ backend/config.js
@@ -1,19 +1,19 @@
 // Read configuration from the environment
 
 // Datbase configruation
-const dbHost = '';
-const dbUser = '';
-const dbPassword = '';
-const dbName = '';
-const dbPort = '';
+const dbHost = process.env.CHOREO_APPDB_HOSTNAME;
+const dbUser = process.env.CHOREO_APPDB_USERNAME;
+const dbPassword = process.env.CHOREO_APPDB_PASSWORD;
+const dbName = process.env.CHOREO_APPDB_DATABASENAME;
+const dbPort = process.env.CHOREO_APPDB_PORT;
 
 // Twilio configruation
-const twilioAccountSid = '';
-const twilioAuthToken = '';
+const twilioAccountSid = process.env.CHOREO_TWILIO2_ACCOUNTSID;
+const twilioAuthToken = process.env.CHOREO_TWILIO2_TOKEN;
 
 // User Manager configruation
-const userManagerServiceUrl = '';
-const userManagerAPIkey = '';
+const userManagerServiceUrl = process.env.CHOREO_UM22_SERVICEURL;
+const userManagerAPIkey = process.env.CHOREO_UM22_CHOREOAPIKEY;
 
 module.exports = {
     dbHost,
