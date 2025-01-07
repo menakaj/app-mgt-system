fdf7c08edbe49bab753553fc4e47abbfb226c280
Menaka Jayawardena <menaka12350@gmail.com>
2025-01-07 11:40:20 +0530

    Update code

--- backend/twilioMessageSender.js
+++ backend/twilioMessageSender.js
@@ -3,13 +3,16 @@ const configs = require('./config');
 
 const { twilioAccountSid, twilioAuthToken } = configs;
 
+console.log(twilioAccountSid, twilioAuthToken);
+
 const client = require('twilio')(twilioAccountSid, twilioAuthToken);
 
 const sendMessage = (userName, service, appointmentDate, phoneNumber) => {
+    console.log(`Sending message to ${phoneNumber} for ${userName} at ${service} on ${appointmentDate}`);
     client.messages
         .create({
             to: phoneNumber,
-            from: '', // TODO Update your twilio phone number here
+            from: '+12312250869', // TODO Update your twilio phone number here
             body: `Hi ${userName}, \nYour appointment for ${service} has been created. \n Date: ${new Date(appointmentDate).toLocaleString()}`,
         }).then(message => console.log(message.sid));
 }
